108a40454d7b518519b0c7d6650efff08fb7075b
peninah98 <peninangizwenayo@gmail.com>
2023-11-06 15:56:08 +0200

    feat: creating header componet:

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-gray-100 font-bold text-center text-3xl leading-10 py-2">Counter App</h1>
+    </div>
+  );
+};
+
+export default Header;
