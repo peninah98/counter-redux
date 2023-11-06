3c8d2166f576cdc0dd3c3a723c74a14e796f3a74
peninah98 <peninangizwenayo@gmail.com>
2023-11-06 16:03:09 +0200

    refactor: adding new style on the outermost div

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Counter from "./components/Counter";
 
 function App() {
   return (
-    <div className="bg-purple-900">
+    <div className="bg-purple-900 w-1/2 mx-auto mt-20 shadow-2xl rounded-sm">
       <Counter />
     </div>
   );
