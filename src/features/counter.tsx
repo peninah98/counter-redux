5f768ab7dc34d7383b72df71c0f4c1bbce6d13bc
peninah98 <peninangizwenayo@gmail.com>
2023-11-06 17:28:33 +0200

    feat: creating the counterSlice function

---
+++ src/features/counter.tsx
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialValue = 0;
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { value: initialValue },
+  reducers: {
+    increment: (state: { value: number }) => {
+      state.value += 1;
+    },
+    decrement: (state: { value: number }) => {
+      state.value -= 1;
+    },
+    reset:(state:{value:number}) => {state.value = initialValue}
+  },
+});
+
+export const {reset,increment,decrement} = counterSlice.actions
+export default counterSlice.reducer
