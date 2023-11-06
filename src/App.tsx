70735b6a954d46a58885001b47a82d3d6fbaee63
peninah98 <peninangizwenayo@gmail.com>
2023-11-06 15:59:45 +0200

    feat: creatimg the completed counter page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
-  return <div className="bg-purple-900"></div>;
+  return (
+    <div className="bg-purple-900">
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
