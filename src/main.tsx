a30617f65279c9b1cd7745e377a9886ec773e617
peninah98 <peninangizwenayo@gmail.com>
2023-11-06 18:36:44 +0200

    feat: adding the store

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
+import {configureStore} from "@reduxjs/toolkit"
+import { Provider } from 'react-redux'
+import reducer from './features/counter.tsx'
+
+const store = configureStore({
+  reducer:{
+    counter : reducer
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
